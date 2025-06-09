be383277cd88da62a355b6f9832b8129caa3eac2
Chispasito <danzivice@gmail.com>
2025-06-09 15:49:05 -0500

    001: Command line input, ai response, and --verbose option

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) <= 1:
+        print("Failure 1: No arguments")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}\n"
+              f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+              f"Response tokens: {response.usage_metadata.cached_content_token_count}"
+        )
+    else: 
+        print(response.text)
+
+main()
